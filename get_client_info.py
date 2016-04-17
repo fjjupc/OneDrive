9861e9b60bc0457370d0b5349aa169b2705f83d7
Carl Fan <fjjupc@gmail.com>
2016-04-17 10:19:45 +0800

    [ADD] Add file for getting client_id and client_secrets

---
+++ get_client_info.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# Author:   Carl Fan
+# Mail:     fjjupc@gmail.com
+# Describe: Get client id & client secret from .config file if it exists. 
+#           Otherwise, get from the user-input.
+
