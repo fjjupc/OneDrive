d2db1cc153e2c88f6499b411f6d240207f1ad06c
Carl Fan <fjjupc@gmail.com>
2016-05-29 11:44:55 +0800

    [CHG] add function list_all: List all file in OneDrive under a specific id("root" for all files in your OneDrive)

--- cmd_tools.py
+++ cmd_tools.py
@@ -6,6 +6,8 @@
 
 from auth import client
 
+N_LIST_DEPTH = 0
+
 def list_id_path(id):
 	items = navigate(id)
 	for count,item in enumerate(items):
@@ -16,4 +18,15 @@ def navigate(item_id):
     items = client.item(id=item_id).children.get()
     return items
 
-list_id_path("root")
+def list_all(id):
+	global N_LIST_DEPTH
+	items = navigate(id)
+	N_LIST_DEPTH += 1
+	for count,item in enumerate(items):
+		print("|-"+(N_LIST_DEPTH-1)*"--"+item.name)
+		list_all(item.id)
+	N_LIST_DEPTH -= 1
+
+N_LIST_DEPTH = 0
+list_all("root")
+
